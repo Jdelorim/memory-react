3b42ca6430a4203c3de0cb9aee443cd3140f908b
Joshua deLorimier <TRASHTRASH_HQ@TRASHTRASHPC.localdomain>
2020-03-24 12:55:55 -0400

    e

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+
+  app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+  })
