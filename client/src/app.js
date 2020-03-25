b94b3801afcc46cd939a20a2118b5f13dfa02988
Joshua <joshuadelorimier@gmail.com>
2020-03-25 11:49:29 -0400

    working on nav bar

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Nav from './components/Nav/Nav';
 import './app.css';
+
+const tempData = {
+    name: 'Joshua deLorimier',
+    score: 40,
+    highScore: 100
+}
+
+
+
 const App = () => {
     
     return(
         <div className='container'>
-           <Nav />
+           <Nav Pname={tempData.name} Pscore={tempData.score} highScore={tempData.highScore} />
         </div>
     )
 }
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './Nav.css';
 
-const Nav = () => {
+const DisplayStats = ({playerName, score, highScore }) => {
+   return(
+    <ul id='menu'>
+        <li>Player Name: {playerName}</li>
+        <li>Player Score: {score}</li>
+        <li>High Score: {highScore}</li>
+    </ul>
+
+   )
+   
+}
+
+
+const Nav = ({Pscore,Pname,highScore}) => {
     return(
         
              <div className='nav-container'>
                 <div className='nav-color-overlay'>
                     <div className='nav-title'>The Expanse Memory Game</div>
-                    
+                    <div className='nav-link-container'>
+                        
+                            <DisplayStats 
+                            playerName={Pname} 
+                            score={Pscore}
+                            highScore={highScore}
+                            />
+                        
+                        
+                    </div>  
                 </div>
             </div>
         
